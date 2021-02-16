f37d32b8d3f2f6cf87b9528373830591fdfc7ed1
nicoleesposo <nicoleesposo.web@gmail.com>
2021-02-16 13:26:32 +0800

    Added demo section and tagline section

--- src/components/Tagline.js
+++ src/components/Tagline.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Container, Row, Col} from 'react-bootstrap'
+
+import Logo from '../assets/images/floqast-logo.png'
 
 const Tagline = () => {
     return(
         <>
-            hi
+            <Container fluid className="tagline">
+                <Row>
+                    <Col className="no-padding" lg={6}>
+                        <div className="tagline__container">
+                            <div className="inner">
+                                <div className="tagline__top mb-1">
+                                    <img className="tagline__img" src={Logo} alt="FloQast Logo" />
+                                    <h1 className="title">The</h1>
+                                </div>
+                                <div className="tagline__middle mb-1">
+                                    <h1 className="title--big text--green">Fastest, Most Accurate</h1>
+                                </div>
+                                <div className="tagline__bottom">
+                                    <h1 className="title">Way to Close Your Books!</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </>
     );
 }
